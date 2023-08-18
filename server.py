3fde4925639e1b3401978f750929b662a74f5e28
Namrata N Keni <namrata.keni14@gmail.com>
2023-08-18 18:32:19 +0530

    changes made to calculator app

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello World!", 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    try:
+        a = request.json['first']
+        b = request.json['second']
+        result = a + b
+        return jsonify({"result": result}), 200
+    except KeyError:
+        return jsonify({"error": "Missing 'first' or 'second' in request data"}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    try:
+        a = request.json['first']
+        b = request.json['second']
+        result = a - b
+        return jsonify({"result": result}), 200
+    except KeyError:
+        return jsonify({"error": "Missing 'first' or 'second' in request data"}), 400
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
+
+
